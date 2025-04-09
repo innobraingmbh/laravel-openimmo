1790416ab7c691fade8611882830a93c29b83340
Bruno Görß <bruno.goerss@inno-brain.de>
2025-04-09 16:14:19 +0200

    fix: annotation for format converter service

--- src/Facades/FormatConverterService.php
+++ src/Facades/FormatConverterService.php
@@ -11,8 +11,8 @@ use Innobrain\OpenImmo\Enums\ConverterDriver;
 /**
  * @see \Innobrain\OpenImmo\Services\FormatConverterService
  *
- * @method static convert(OpenImmo $openImmo)
- * @method static driver(ConverterDriver $driver)
+ * @method static array convert(OpenImmo $openImmo)
+ * @method static \Innobrain\OpenImmo\Services\FormatConverterService driver(ConverterDriver $driver)
  */
 class FormatConverterService extends Facade
 {
