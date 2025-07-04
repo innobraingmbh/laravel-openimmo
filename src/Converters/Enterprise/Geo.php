3bf030c122bd7ccf2266c0567850f7106363effc
Konstantin Auffinger <62616071+kauffinger@users.noreply.github.com>
2025-07-04 14:54:12 +0200

    fix: convert two letter to three letter country codes

--- src/Converters/Enterprise/Geo.php
+++ src/Converters/Enterprise/Geo.php
@@ -10,13 +10,61 @@ trait Geo
     {
         $geo = getGeo($this->openImmo);
 
+        $countryCode = $geo->getCountry()?->getIsoCountry();
+
+        // Convert 2-letter country codes to 3-letter codes
+        if ($countryCode !== null && strlen($countryCode) === 2) {
+            $countryMap = [
+                'DE' => 'DEU',
+                'AT' => 'AUT',
+                'FR' => 'FRA',
+                'CH' => 'CHE',
+                'IT' => 'ITA',
+                'ES' => 'ESP',
+                'GB' => 'GBR',
+                'NL' => 'NLD',
+                'BE' => 'BEL',
+                'LU' => 'LUX',
+                'PL' => 'POL',
+                'CZ' => 'CZE',
+                'SK' => 'SVK',
+                'HU' => 'HUN',
+                'SI' => 'SVN',
+                'HR' => 'HRV',
+                'RO' => 'ROM',
+                'BG' => 'BGR',
+                'GR' => 'GRC',
+                'PT' => 'PRT',
+                'SE' => 'SWE',
+                'DK' => 'DNK',
+                'NO' => 'NOR',
+                'FI' => 'FIN',
+                'US' => 'USA',
+                'CA' => 'CAN',
+                'AU' => 'AUS',
+                'NZ' => 'NZL',
+                'JP' => 'JPN',
+                'CN' => 'CHN',
+                'IN' => 'IND',
+                'BR' => 'BRA',
+                'MX' => 'MEX',
+                'RU' => 'RUS',
+                'TR' => 'TUR',
+                'AE' => 'ARE',
+                'IL' => 'ISR',
+                'ZA' => 'ZAF',
+            ];
+
+            $countryCode = $countryMap[strtoupper($countryCode)] ?? $countryCode;
+        }
+
         return [
             'plz' => $geo->getPostalCode(),
             'ort' => $geo->getCity(),
             'strasse' => $geo->getStreet(),
             'hausnummer' => $geo->getHouseNumber(),
             'bundesland' => $geo->getState(),
-            'land' => $geo->getCountry()?->getIsoCountry(),
+            'land' => $countryCode,
             'etage' => $geo->getFloor(),
             'flur' => $geo->getCorridor(),
             'flurstueck' => $geo->getParcel(),
