e1b8312230f7f43a799fcfd175d89fd9b1a4a385
Bruno Görß <bruno.goerss@inno-brain.de>
2025-10-13 10:03:34 +0200

    ci: change rector rules

--- rector.php
+++ rector.php
@@ -5,31 +5,35 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use RectorLaravel\Rector\Expr\AppEnvironmentComparisonToParameterRector;
 use RectorLaravel\Rector\FuncCall\RemoveDumpDataDeadCodeRector;
 use RectorLaravel\Set\LaravelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__.'/config',
-        __DIR__.'/src',
-        __DIR__.'/tests',
-        __DIR__.'/skeleton',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-    $rectorConfig->rule(RemoveDumpDataDeadCodeRector::class);
-    $rectorConfig->rule(AppEnvironmentComparisonToParameterRector::class);
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-        SetList::DEAD_CODE,
-        SetList::CODE_QUALITY,
-        SetList::EARLY_RETURN,
-        LaravelSetList::LARAVEL_110,
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withRules([
+        RemoveDumpDataDeadCodeRector::class,
+        AppEnvironmentComparisonToParameterRector::class,
+    ])
+    ->withSets([
+        LevelSetList::UP_TO_PHP_84,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+        LaravelSetList::LARAVEL_120,
         LaravelSetList::LARAVEL_IF_HELPERS,
         LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
     ]);
-};
